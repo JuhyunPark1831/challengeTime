80f94a25d23e160fd07ac10380e5340615db3eab
JuhyunPark1831 <epahs1831@naver.com>
2024-03-04 04:02:44 +0900

    Init: FE

--- src/main/java/com/sideProject/challengeTime/domain/MainController.java
+++ src/main/java/com/sideProject/challengeTime/domain/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/api/hello")
     public String mainPage() {
         return "main 페이지";
     }
